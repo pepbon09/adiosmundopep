9187989c07abab15bfd4a1036e278594b8439076
Pep Bonafont <bonafontpep@gmail.com>
2019-09-26 20:29:55 +0200

    Version 1.1

--- adiosmundo.java
+++ adiosmundo.java
@@ -1,5 +1,6 @@
 public class adiosmundo { //Clase principal
   public static void main(String[] args) {
     System.out.println("AdIoS mUnDo CrUeL");
+    System.out.println("Nos vamos don fernando, Pero volveremo");
     }
 }
