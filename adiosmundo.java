c4ff13502613a1ff748af5c99da0167570566896
Pep Bonafont <bonafontpep@gmail.com>
2019-10-03 18:28:36 +0200

    Experimento colorea2

--- adiosmundo.java
+++ adiosmundo.java
@@ -2,7 +2,8 @@ public class adiosmundo { //Clase principal
   public static void main(String[] args) {
 	String amarillo = "\033[33m";
 	String verde = "\033[32m";
+	String reset = "\033[0m";
 		System.out.println(amarillo+"AdIoS mUnDo CrUeL");
-		System.out.println(verde+"Nos vamos don fernando, Pero volveremo");
+		System.out.println(verde+"Nos vamos don fernando, Pero volveremo"+reset);
     }
 }
